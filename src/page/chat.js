d24cf991ce8087a5b5cf8036041856c3a97dc698
zjy <jinyou.zeng@wukongsch.com>
2023-07-31 13:56:34 +0800

    新增enter提交输入内容

--- src/page/chat.js
+++ src/page/chat.js
@@ -1,66 +1,82 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function ChatMessages(props) {
-
-  const [inputText, setInputText] = useState('');
-  const [status, setStatus] = useState('idle');
+  const [inputText, setInputText] = useState("");
+  const [status, setStatus] = useState("idle");
 
   const submitText = () => {
     // let url = `https://d9wqfr-8000.csb.app/claude/append_message?conversation_uuid=${props.uuid}&prompt=${inputText}`;
-    let url ='https://d9wqfr-8000.csb.app/claude/chat_message'
-    props.onMessage({'text':inputText,'sender':'human'})
-    setInputText('')
-    setStatus('pending'); 
+    let url = "https://d9wqfr-8000.csb.app/claude/chat_message";
+    props.onMessage({ text: inputText, sender: "human" });
+    setInputText("");
+    setStatus("pending");
     const options = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({conversation_uuid:props.uuid,prompt:inputText})
+      body: JSON.stringify({
+        conversation_uuid: props.uuid,
+        prompt: inputText,
+      }),
     };
-    fetch(url,options)
-      .then(res => res.json())
-      .then(data => {props.onMessage({'text':data.data,'sender':'assistant'}) ;setStatus('success'); }
-      )
-      .catch(() => setStatus('error'));
-     
-  }
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((data) => {
+        props.onMessage({ text: data.data, sender: "assistant" });
+        setStatus("success");
+      })
+      .catch(() => setStatus("error"));
+  };
   return (
-    <div style={{
-        display: 'flex',
-        width:'33%',
-        height:'50px',
-        justifyContent: 'center',
-        position: 'absolute',
-        alignItems: 'center',
-        left: '31%',
-        padding:'10px',
-        bottom: '0',
-        zIndex: '999'
-      }}>
-      <textarea 
-        type="text" 
+    <div
+      style={{
+        display: "flex",
+        width: "33%",
+        height: "50px",
+        justifyContent: "center",
+        position: "absolute",
+        alignItems: "center",
+        left: "31%",
+        padding: "10px",
+        bottom: "0",
+        zIndex: "999",
+      }}
+    >
+      <textarea
+        type="text"
         value={inputText}
-        onChange={e => setInputText(e.target.value)} 
+        onChange={(e) => setInputText(e.target.value)}
         style={{
-            width: 'auto',
-            flexGrow: 1, 
-            minWidth: 200,
-            fontSize:'large',
-            margin:'5px',
-            border: '1px solid black',
-            borderRadius: '10px' ,
-          }}
+          width: "auto",
+          flexGrow: 1,
+          minWidth: 200,
+          fontSize: "large",
+          margin: "5px",
+          border: "1px solid black",
+          borderRadius: "10px",
+        }}
+        onKeyDown={(e) => {
+          if (
+            e.key === "Enter" &&
+            e.ctrlKey === false &&
+            e.shiftKey === false &&
+            e.altKey === false
+          ) {
+            submitText();
+          }
+        }}
       />
 
-      <button onClick={submitText}>发送</button> 
-      <div style={{padding: '5px 10px'}}> {status === 'pending' && <div>等待中...</div>}
-      {status === 'success' && <div>发送成功!</div>}
-      {status === 'error' && <div>发送失败!</div>}</div>
-      
+      <button onClick={submitText}>发送</button>
+      <div style={{ padding: "5px 10px" }}>
+        {" "}
+        {status === "pending" && <div>等待中...</div>}
+        {status === "success" && <div>发送成功!</div>}
+        {status === "error" && <div>发送失败!</div>}
+      </div>
     </div>
   );
-
 }
 
 export default ChatMessages;
